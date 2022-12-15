c3842941c9d8105063683bc0fce18ea9d03f92a8
nakazon1030 <nakazon1111@gmail.com>
2022-12-15 13:55:02 +0900

    finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, jsonify
 from werkzeug.security import generate_password_hash as gph
 from werkzeug.security import check_password_hash as cph
 from datetime import timedelta
@@ -8,6 +8,7 @@ import html
 import datetime 
 import secrets
 import numpy as np
+import dicttoxml
 
 def connect():
     con = MySQLdb.connect(
@@ -23,6 +24,12 @@ def connect():
 app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe(16)
 app.permanent_session_lifetime = timedelta(minutes=60)
+app.config['JSON_AS_ASCII'] = False
+
+@app.after_request
+def apply_caching(response):
+    response.headers["X-Frame-Options"] = "SAMEORIGIN"
+    return response
 
 @app.route("/")
 @app.route("/index")
@@ -36,12 +43,12 @@ def register():
         return render_template("register.html", msg="新規登録をしてください")
 
     elif request.method == "POST":
-        user_id = request.form.get("user_id")
-        name = request.form.get("name")
-        password = request.form.get("password")
-        password_sam = request.form.get("password_sam")
-        group_id = request.form.get("group_id")
-        group_password = request.form.get("group_password")
+        user_id = html.escape(request.form.get("user_id"))
+        name = html.escape(request.form.get("name"))
+        password = html.escape(request.form.get("password"))
+        password_sam = html.escape(request.form.get("password_sam"))
+        group_name = html.escape(request.form.get("group_name"))
+        group_password = html.escape(request.form.get("group_password"))
 
         if password == password_sam:
             hashpass = gph(password)
@@ -54,7 +61,7 @@ def register():
             for row in cur:
                 data.append(row)
             if len(data)!=0:
-                return render_template(register.html, msg="すでに使用されているユーザー名です。")
+                return render_template("register.html", msg="すでに使用されているユーザー名です。")
             con.commit()
             con.close()
 
@@ -63,8 +70,8 @@ def register():
             cur = con.cursor()
             cur.execute("""SELECT pass 
                           FROM grouplist
-                          WHERE group_id = %(id)s
-                        """, {"id":group_id})
+                          WHERE group_name = %(id)s
+                        """, {"id":group_name})
             data=[]
             for row in cur:
                 data.append(row[0])
@@ -84,27 +91,17 @@ def register():
             cur = con.cursor()
             cur.execute("""
                         INSERT INTO user
-                        (user_id,user_name,pass,group_id)
-                        VALUES (%(user_id)s, %(name)s, %(pass)s,%(group_id)s)
-                        """,{"user_id":user_id, "name":name, "pass":hashpass, "group_id":group_id} )
+                        (user_id,user_name,pass,group_name)
+                        VALUES (%(user_id)s, %(name)s, %(pass)s,%(group_name)s)
+                        """,{"user_id":user_id, "name":name, "pass":hashpass, "group_name":group_name} )
             con.commit()   
             con.close()
 
-            con = connect()
-            cur = con.cursor()
-            cur.execute("""SELECT group_name 
-                        FROM grouplist
-                        WHERE group_id = %(id)s
-                    """, {"id":group_id})
-            
-            group_name = row
-            con.commit()   
-            con.close()
 
-            session["group_id"] = group_id
+            session["group_name"] = group_name
             session["user_id"] = user_id
             session["name"] = name
-            session["group_name"] = group_name
+
 
 
             return redirect("/home")
@@ -123,10 +120,9 @@ def group_register():
         return render_template("gregister.html", msg="新規登録をしてください")
 
     elif request.method == "POST":
-        group_id = request.form.get("group_id")
-        group_name = request.form.get("group_name")
-        password = request.form.get("password")
-        password_sam = request.form.get("password_sam")
+        group_name = html.escape(request.form.get("group_name"))
+        password = html.escape(request.form.get("password"))
+        password_sam = html.escape(request.form.get("password_sam"))
 
         if password == password_sam:
             hashpass = gph(password)
@@ -134,13 +130,13 @@ def group_register():
             con = connect()
             cur = con.cursor()
             cur.execute("""
-                        SELECT * FROM grouplist WHERE group_id=%(id)s
-                        """, {"id":group_id})
+                        SELECT * FROM grouplist WHERE group_name=%(id)s
+                        """, {"id":group_name})
             data = []
             for row in cur:
                 data.append(row)
             if len(data)!=0:
-                return render_template("gregister.html", msg="すでに存在するメールアドレスです")
+                return render_template("gregister.html", msg="すでに存在する組織名です")
             con.commit()
             con.close()
 
@@ -148,9 +144,9 @@ def group_register():
             cur = con.cursor()
             cur.execute("""
                         INSERT INTO grouplist
-                        (group_id,group_name,pass)
-                        VALUES (%(id)s, %(name)s, %(pass)s)
-                        """,{"id":group_id, "name":group_name, "pass":hashpass} )
+                        (group_name,pass)
+                        VALUES (%(id)s, %(pass)s)
+                        """,{"id":group_name, "pass":hashpass} )
 
             con.commit()
             con.close()
@@ -166,8 +162,8 @@ def login():
         session.clear()
         return render_template("login.html")
     elif request.method == "POST":
-        user_id = request.form.get("user_id")
-        password = request.form.get("password")
+        user_id = html.escape(request.form.get("user_id"))
+        password = html.escape(request.form.get("password"))
         con = connect()
         cur = con.cursor()
         cur.execute("""SELECT pass 
@@ -197,30 +193,18 @@ def login():
             
             con = connect()
             cur = con.cursor()
-            cur.execute("""SELECT group_id 
+            cur.execute("""SELECT group_name 
                         FROM user
                         WHERE user_id = %(id)s
                     """, {"id":user_id})
-            for row in cur:
-                group_id = row[0]
-            con.commit()   
-            con.close()
-
-            con = connect()
-            cur = con.cursor()
-            cur.execute("""SELECT group_name
-                        FROM grouplist
-                        WHERE group_id = %(id)s
-                    """, {"id":group_id})
             for row in cur:
                 group_name = row[0]
             con.commit()   
             con.close()
 
-            session["group_id"] = group_id
+            session["group_name"] = group_name
             session["user_id"] = user_id
             session["name"] = name
-            session["group_name"] = group_name
 
             return redirect("/home")
         else:
@@ -231,13 +215,13 @@ def login():
 @app.route("/home", methods=["GET"])
 def home():
     #タスク表示
-    if "group_id" in session:
+    if "group_name" in session:
         group_name = session["group_name"]
         con = connect()
         cur = con.cursor()
         cur.execute("""SELECT *
                         FROM task
-                        WHERE group_id = %(id)s
+                        WHERE group_name = %(id)s
                     """, {"id":group_name})
         data = [] 
         for row in cur:
@@ -251,22 +235,22 @@ def home():
 @app.route("/create", methods=["GET","POST"])
 def create():
     ##タスク追加処理
-    if "group_id" in session:
+    if "group_name" in session:
         if request.method == "GET":
             return render_template("create.html", msg="タスクを登録してください")
 
         elif request.method == "POST":
-            name = request.form.get("name")
-            body = request.form.get("body")
+            name = html.escape(request.form.get("name"))
+            body = html.escape(request.form.get("body"))
             date = datetime.date.today()
-            group_id = session["group_id"]
+            group_name = session["group_name"]
             con = connect()
             cur = con.cursor()
             cur.execute("""
                         INSERT INTO task
-                        (group_id,task_name,body,date,status)
+                        (group_name,task_name,body,date,status)
                         VALUES (%(id)s,%(name)s, %(body)s, %(date)s,0)
-                        """,{"id":group_id,"name":name, "body":body, "date":date} )
+                        """,{"id":group_name,"name":name, "body":body, "date":date} )
 
             con.commit()
             con.close()
@@ -277,97 +261,148 @@ def create():
 @app.route("/edit/<int:id>", methods=["GET","POST"])
 def edit(id):
     ##タスク更新処理
-    if "group_id" in session:
-        if request.method == "GET":
-
-            con = connect()
-            cur = con.cursor()
-            cur.execute("""SELECT *
-                            FROM task
-                            WHERE task_id = %(id)s
-                        """, {"id":id})
+    if "group_name" in session:
+        con = connect()
+        cur = con.cursor()
+        cur.execute("""SELECT group_name
+                        FROM task
+                        WHERE task_id = %(id)s
+                    """, {"id":id})
             
-            for row in cur:
-                title = row[2]
-                body = row[3]
-            
-            con.close()
-            return render_template("edit.html", msg="タスクリストを編集してください", title = title, body = body, id = id)
+        for row in cur:
+            group_name = row[0]
+        con.close()
+        if session["group_name"] == group_name:
+            if request.method == "GET":
 
-        elif request.method == "POST":
-            name = request.form.get("name")
-            body = request.form.get("body")
-            date = datetime.date.today()
+                con = connect()
+                cur = con.cursor()
+                cur.execute("""SELECT *
+                                FROM task
+                                WHERE task_id = %(id)s
+                            """, {"id":id})
+                
+                for row in cur:
+                    title = row[2]
+                    body = row[3]
+                
+                con.close()
+                return render_template("edit.html", msg="タスクリストを編集してください", title = title, body = body, id = id)
 
-            con = connect()
-            cur = con.cursor()
-            cur.execute("""
-                        UPDATE task
-                        SET    task_name = %(name)s, body = %(body)s, date = %(date)s
-                        WHERE  task_id = %(id)s
-                        """,{"name":name, "body":body, "date":date, "id":id})
+            elif request.method == "POST":
+                name = html.escape(request.form.get("name"))
+                body = html.escape(request.form.get("body"))
+                date = datetime.date.today()
 
-            con.commit()
-            con.close()
-        return redirect("/home")
+                con = connect()
+                cur = con.cursor()
+                cur.execute("""
+                            UPDATE task
+                            SET    task_name = %(name)s, body = %(body)s, date = %(date)s
+                            WHERE  task_id = %(id)s
+                            """,{"name":name, "body":body, "date":date, "id":id})
+
+                con.commit()
+                con.close()
+            return redirect("/home")
+        else:
+            return redirect("/home")
     else:
         redirect("/")
 
 @app.route("/status1/<int:id>", methods=["GET"])
 def status1(id):
     ##タスク完了処理
-    if "group_id" in session:
-        date = datetime.date.today()
-
+    if "group_name" in session:
         con = connect()
         cur = con.cursor()
-        cur.execute("""
-                        UPDATE task
-                        SET    status = "1", date = %(date)s
-                        WHERE  task_id = %(id)s
-                    """,{"date":date, "id":id})
-
-        con.commit()
+        cur.execute("""SELECT group_name
+                        FROM task
+                        WHERE task_id = %(id)s
+                    """, {"id":id})
+            
+        for row in cur:
+            group_name = row[0]
         con.close()
-        return redirect("/home")
+        if session["group_name"] == group_name:
+            date = datetime.date.today()
+
+            con = connect()
+            cur = con.cursor()
+            cur.execute("""
+                            UPDATE task
+                            SET    status = "1", date = %(date)s
+                            WHERE  task_id = %(id)s
+                        """,{"date":date, "id":id})
+
+            con.commit()
+            con.close()
+            return redirect("/home")
+        else:
+            return redirect("/home")
     else :
         return redirect("/")
 
 @app.route("/status0/<int:id>", methods=["GET"])
 def status0(id):
     ##タスク未完了処理
-    if "group_id" in session:
-        date = datetime.date.today()
-
+    if "group_name" in session:
         con = connect()
         cur = con.cursor()
-        cur.execute("""
-                        UPDATE task
-                        SET    status = "0", date = %(date)s
-                        WHERE  task_id = %(id)s
-                    """,{"date":date, "id":id})
-
-        con.commit()
+        cur.execute("""SELECT group_name
+                        FROM task
+                        WHERE task_id = %(id)s
+                    """, {"id":id})
+            
+        for row in cur:
+            group_name = row[0]
         con.close()
-        return redirect("/home")
+        if session["group_name"] == group_name:
+            date = datetime.date.today()
+
+            con = connect()
+            cur = con.cursor()
+            cur.execute("""
+                            UPDATE task
+                            SET    status = "0", date = %(date)s
+                            WHERE  task_id = %(id)s
+                        """,{"date":date, "id":id})
+
+            con.commit()
+            con.close()
+            return redirect("/home")
+        else:
+            return redirect("/home")
     else :
         return redirect("/")
 
 @app.route("/delete/<int:id>", methods=["GET"])
 def delete(id):
     ##タスク未完了処理
-    if "group_id" in session:
-
+    if "group_name" in session:
         con = connect()
         cur = con.cursor()
-        cur.execute("""
-                        DELETE FROM task
-                        WHERE  task_id = %(id)s
-                    """,{"id":id})
-
-        con.commit()
+        cur.execute("""SELECT group_name
+                        FROM task
+                        WHERE task_id = %(id)s
+                    """, {"id":id})
+            
+        for row in cur:
+            group_name = row[0]
         con.close()
-        return redirect("/home")
+        if session["group_name"] == group_name:
+            con = connect()
+            cur = con.cursor()
+            cur.execute("""
+                            DELETE FROM task
+                            WHERE  task_id = %(id)s
+                        """,{"id":id})
+
+            con.commit()
+            con.close()
+            return redirect("/home")
+        else :
+            return redirect("/home")
     else :
         return redirect("/")
 @app.route("/logout")
@@ -375,6 +410,58 @@ def logout():
     #ログアウト処理
     session.clear()
     return redirect("/")
+
+@app.route("/json/<group_name>", methods=["GET"])
+def json(group_name):
+    ##json出力処理
+    con = connect()
+    cur = con.cursor()
+    cur.execute("""SELECT *
+                    FROM task
+                    WHERE group_name = %(id)s
+                """, {"id":group_name})
+    tasklist = [] 
+    for row in cur:
+        tasklist.append(row)
+    con.close()
+    dic = {}
+    dic["team_name"] = group_name
+
+    for task in tasklist:
+        dic[task[2]+"_name"] = task[2]
+        dic[task[2]+"_body"] = task[3]
+        dic[task[2]+"_date"] = task[4]
+        dic[task[2]+"_status"] = task[5]
+
+    return jsonify(dic)
+
+@app.route("/xml/<group_name>", methods=["GET"])
+def xml(group_name):
+    ##xml出力処理 
+    con = connect()
+    cur = con.cursor()
+    cur.execute("""SELECT *
+                    FROM task
+                    WHERE group_name = %(id)s
+                """, {"id":group_name})
+    tasklist = [] 
+    for row in cur:
+        tasklist.append(row)
+    con.close()
+    dic = {}
+    dic["team_name"] = group_name
+
+    for task in tasklist:
+        dic[task[2]+"_name"] = task[2]
+        dic[task[2]+"_body"] = task[3]
+        dic[task[2]+"_date"] = task[4]
+        dic[task[2]+"_status"] = task[5]
+    
+    xml = dicttoxml.dicttoxml(dic)
+    resp = app.make_response(xml)
+    resp.mimetype = "text/xml"
+
+    return resp
 
 
 if __name__ == "__main__":
